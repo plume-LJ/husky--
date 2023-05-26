e27bc7d8f715e7a688303be0f19e3353588f3f4b
plume zhou <jmyumao@126.com>
2023-05-26 18:29:16 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { map } from "ramda";
 console.log(map((i) => i)([1, 2, 3]));
 console.log(map((i) => i)([1, 2, 3]));
 
-function T(a: any) {
+const a = "s";
+console.log(a);
+function T(a: string | number) {
 	return a;
 }
 
 console.log(T(2));
+console.log(T("2"));
